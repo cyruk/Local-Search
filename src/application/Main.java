0b9b5221cbc5b472c4dfc424b10a68cfb3c94ea5
jkrasnits <jkrasnits@gmail.com>
2017-09-22 15:52:06 -0400

    uncommented code that i was testing

--- src/application/Main.java
+++ src/application/Main.java
@@ -25,63 +25,54 @@ public class Main extends Application {
 		dialog.setTitle("Dimensions");
 		dialog.setHeaderText("What are the dimensions of your NxN square");
 		dialog.setContentText("Please enter your N:");
+		//Add way to load file and choose type of search
 
 		Optional<String> result = dialog.showAndWait();
 		int n = Integer.valueOf(result.get());
 
 		String searchType = "Simulated Annealing";
 		long startTime = System.nanoTime();
+		//Need a way to choose which type of search from the dialogue
 		Grid gridStruct = this.simulatedAnnealing(5000, 2, .8, n);
 		long endTime = System.nanoTime();
 		int gridValue = gridStruct.evaluate();
-
 		int searchTime = (int) ((endTime - startTime)/1000000);
-		//System.out.println(searchType + " | N: "+n+ " | Value: "+gridValue+ " | Search Time: " + searchTime+ "ms");
 
-//		GridPane grid = new GridPane();
-//
-//	    for (int row = 0; row < n; row++) {
-//	        for (int col = 0; col < n; col++) {
-//	        	Text numberText;
-//	            Rectangle rec = new Rectangle();
-//	            rec.setWidth(40);
-//	            rec.setHeight(40);
-//
-//	            if(gridStruct.getCell(row, col).depth == 0 && gridStruct.getCell(row, col).isVisited){
-//	            	rec.setFill(Color.LIGHTBLUE);
-//	            }else if(gridStruct.getCell(row, col).isVisited){
-//	            	rec.setFill(Color.GREEN);
-//	            }else{
-//	            	rec.setFill(Color.RED);
-//	            }
-//	            rec.setStroke(Color.BLACK);
-//
-//	            //Create container to be able to hold text
-//	            StackPane pane = new StackPane();
-//	            numberText = new Text(gridStruct.getCell(row, col).value+":"+gridStruct.getCell(row, col).depth);
-//
-//	            //add in text
-//	            pane.getChildren().addAll(rec, numberText);
-//	            GridPane.setRowIndex(pane, row);
-//	            GridPane.setColumnIndex(pane, col);
-//	            grid.getChildren().add(pane);
-//	        }
-//	    }
-//
-//	    Scene scene = new Scene(grid, 500, 500);
-//
-//	    primaryStage.setTitle(searchType + " | N: "+n+ " | Value: "+gridValue+ " | Search Time: " + searchTime+ "ms");
-//	    primaryStage.setScene(scene);
-//	    primaryStage.show();
+		GridPane grid = new GridPane();
 
-	    //this evaluates the shortest number of moves from the given start to the end
-	    //System.out.println(gridStruct);
-	    //System.out.println(gridStruct.evaluate());
+	    for (int row = 0; row < n; row++) {
+	        for (int col = 0; col < n; col++) {
+	        	Text numberText;
+	            Rectangle rec = new Rectangle();
+	            rec.setWidth(40);
+	            rec.setHeight(40);
 
-	    //System.out.println(this.basicHillClimb(100000, n).evaluate());
-	    //System.out.println(this.hillClimbWithRandomRestarts(100, 1000, n).evaluate());
-	    //System.out.println(this.hillClimbWithRandomWalk(.01, 5000, n).evaluate());
-	    //System.out.println(this.simulatedAnnealing(5000, 10, .95, n).evaluate());
+	            if(gridStruct.getCell(row, col).depth == 0 && gridStruct.getCell(row, col).isVisited){
+	            	rec.setFill(Color.LIGHTBLUE);
+	            }else if(gridStruct.getCell(row, col).isVisited){
+	            	rec.setFill(Color.GREEN);
+	            }else{
+	            	rec.setFill(Color.RED);
+	            }
+	            rec.setStroke(Color.BLACK);
+
+	            //Create container to be able to hold text
+	            StackPane pane = new StackPane();
+	            numberText = new Text(gridStruct.getCell(row, col).value+":"+gridStruct.getCell(row, col).depth);
+
+	            //add in text
+	            pane.getChildren().addAll(rec, numberText);
+	            GridPane.setRowIndex(pane, row);
+	            GridPane.setColumnIndex(pane, col);
+	            grid.getChildren().add(pane);
+	        }
+	    }
+
+	    Scene scene = new Scene(grid, 500, 500);
+
+	    primaryStage.setTitle(searchType + " | N: "+n+ " | Value: "+gridValue+ " | Search Time: " + searchTime+ "ms");
+	    primaryStage.setScene(scene);
+	    primaryStage.show();
 
 		printTestGrids();
 		//this.testAllSearches();
@@ -390,45 +381,45 @@ public class Main extends Application {
 
 	public void testAllSearches(){
 	    //Testing code
-//		System.out.println("Basic Hill Climbing");
-//	    System.out.println("Iter\t5x5\t7x7\t9x9\t11x11");
-//	    for(int i = 0; i <= 5000; i+=100){
-//	    	int total5 = 0;
-//	    	int total7 = 0;
-//	    	int total9 = 0;
-//	    	int total11 = 0;
-//	    	for(int j = 0; j < 50; j++){
-//	    		total5 += this.basicHillClimb(i, 5).evaluate();
-//	    		total7 += this.basicHillClimb(i, 7).evaluate();
-//	    		total9 += this.basicHillClimb(i, 9).evaluate();
-//	    		total11 += this.basicHillClimb(i, 11).evaluate();
-//	    	}
-//	    	int avgForIterations5 = total5/50;
-//	    	int avgForIterations7 = total7/50;
-//	    	int avgForIterations9 = total9/50;
-//	    	int avgForIterations11 = total11/50;
-//	    	System.out.println(i + "\t" + avgForIterations5 + "\t" + avgForIterations7 + "\t" + avgForIterations9 + "\t" + avgForIterations11);
-//	    }
-//
-//  	    System.out.println("Hill Climb With Random Restarts (50 restarts)");
-//	    System.out.println("Iter\t5x5\t7x7\t9x9\t11x11");
-//	    for(int i = 0; i <= 5000; i+=100){
-//	    	int total5 = 0;
-//	    	int total7 = 0;
-//	    	int total9 = 0;
-//	    	int total11 = 0;
-//	    	for(int j = 0; j < 50; j++){
-//	    		total5 += this.hillClimbWithRandomRestarts(50, i/50, 5).evaluate();
-//	    		total7 += this.hillClimbWithRandomRestarts(50, i/50, 7).evaluate();
-//	    		total9 += this.hillClimbWithRandomRestarts(50, i/50, 9).evaluate();
-//	    		total11 += this.hillClimbWithRandomRestarts(50, i/50, 11).evaluate();
-//	    	}
-//	    	int avgForIterations5 = total5/50;
-//	    	int avgForIterations7 = total7/50;
-//	    	int avgForIterations9 = total9/50;
-//	    	int avgForIterations11 = total11/50;
-//	    	System.out.println(i + "\t" + avgForIterations5 + "\t" + avgForIterations7 + "\t" + avgForIterations9 + "\t" + avgForIterations11);
-//	    }
+		System.out.println("Basic Hill Climbing");
+	    System.out.println("Iter\t5x5\t7x7\t9x9\t11x11");
+	    for(int i = 0; i <= 5000; i+=100){
+	    	int total5 = 0;
+	    	int total7 = 0;
+	    	int total9 = 0;
+	    	int total11 = 0;
+	    	for(int j = 0; j < 50; j++){
+	    		total5 += this.basicHillClimb(i, 5).evaluate();
+	    		total7 += this.basicHillClimb(i, 7).evaluate();
+	    		total9 += this.basicHillClimb(i, 9).evaluate();
+	    		total11 += this.basicHillClimb(i, 11).evaluate();
+	    	}
+	    	int avgForIterations5 = total5/50;
+	    	int avgForIterations7 = total7/50;
+	    	int avgForIterations9 = total9/50;
+	    	int avgForIterations11 = total11/50;
+	    	System.out.println(i + "\t" + avgForIterations5 + "\t" + avgForIterations7 + "\t" + avgForIterations9 + "\t" + avgForIterations11);
+	    }
+
+  	    System.out.println("Hill Climb With Random Restarts (50 restarts)");
+	    System.out.println("Iter\t5x5\t7x7\t9x9\t11x11");
+	    for(int i = 0; i <= 5000; i+=100){
+	    	int total5 = 0;
+	    	int total7 = 0;
+	    	int total9 = 0;
+	    	int total11 = 0;
+	    	for(int j = 0; j < 50; j++){
+	    		total5 += this.hillClimbWithRandomRestarts(50, i/50, 5).evaluate();
+	    		total7 += this.hillClimbWithRandomRestarts(50, i/50, 7).evaluate();
+	    		total9 += this.hillClimbWithRandomRestarts(50, i/50, 9).evaluate();
+	    		total11 += this.hillClimbWithRandomRestarts(50, i/50, 11).evaluate();
+	    	}
+	    	int avgForIterations5 = total5/50;
+	    	int avgForIterations7 = total7/50;
+	    	int avgForIterations9 = total9/50;
+	    	int avgForIterations11 = total11/50;
+	    	System.out.println(i + "\t" + avgForIterations5 + "\t" + avgForIterations7 + "\t" + avgForIterations9 + "\t" + avgForIterations11);
+	    }
 
 	    System.out.println("Hill Climb with Random Walk (p = .01)");
 	    System.out.println("Iter\t5x5\t7x7\t9x9\t11x11");
